57573ba2320b7296d4f685c40c131d205743d00b
n3manj4 <nemanjaks4@gmail.com>
2022-07-21 19:27:47 +0200

    Start implementing Ordering.Application

---
+++ src/Services/Ordering/Ordering.Application/Models/Email.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Ordering.Application.Models
+{
+	internal class Email
+	{
+		public string To { get; set; }
+		public string Subject { get; set; }
+		public string Body { get; set; }
+	}
+}
